16bafcaced000f9f625ba87e48629a10bf5baff5
Petrkoul <koulpetran@yahoo.gr>
2023-11-15 13:34:39 +0200

    first-commit

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,40 @@
+// import { Component } from "react";
+import Card from "../card/card";
+import './card-list.styles.css'
+
+
+
+const CardList = ({monsters}) => (
+
+    <div className="card-list">
+        {monsters.map((monster)=>{
+            return <Card 
+                id={monster.id} 
+                name={monster.name}
+                email={monster.email}
+            />
+        })}
+    </div>
+)    
+
+
+
+
+// class CardList extends Component {
+//     render(){
+        
+//         const {monsters} = this.props;
+        
+//         return (
+//             <div className="card-list">
+//                 {monsters.map((monster) => {
+                    
+//                     return (
+//                         <Card monster={monster}/>
+//                 )})}
+//             </div>
+//         )
+//     }
+// }
+
+export default CardList;
